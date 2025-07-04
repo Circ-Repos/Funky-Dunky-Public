b6d7652bbc946372ce504bdc6a3dfe639157ab66
Circuitella <116322307+Circuitella@users.noreply.github.com>
2025-07-04 02:08:51 -0400

    otion

--- data/states/custom/options/ControlsOptions.hx
+++ data/states/custom/options/ControlsOptions.hx
@@ -10,14 +10,24 @@ var p2ControlsNotes:Array = [];
 var page:Int = 0;
 
 var optionNum:Int = 0;
-var optionItemFix:Int = 0;
+var scrollOffset:Int = 0;
+var visibleRows:Int = 6;
 
-var shitsA:Array<String> = ['NOTE_LEFT', 'NOTE_DOWN', 'NOTE_UP', 'NOTE_RIGHT'];
-var shitsB:Array<String> = ['LEFT', 'DOWN', 'UP', 'RIGHT', 'ACCEPT', 'BACK'];
-var songOffsetSetter:Int = 0;
+var shitsA:Array<String> = [
+    'NOTE_LEFT', 'NOTE_DOWN', 'NOTE_UP', 'NOTE_RIGHT',
+    'LEFT', 'DOWN', 'UP', 'RIGHT',
+    'ACCEPT', 'BACK'
+];
+
+var shitsB:Array<String> = [
+    'NOTE LEFT', 'NOTE DOWN', 'NOTE UP', 'NOTE RIGHT',
+    'UI LEFT', 'UI DOWN', 'UI UP', 'UI RIGHT',
+    'ACCEPT', 'BACK'
+];
+
 var helper:Array = ['←', '⌄', '^', '→'];
 
-function create(){
+function create() {
     bg = new FlxSprite(0, 0).makeGraphic(FlxG.width, FlxG.height, 0xFF6617B5);
     insert(0, bg);
 
@@ -25,29 +35,23 @@ function create(){
     topLayer.alpha = 0.5;
     insert(100, topLayer);
 
-    selectedOption =  new FlxSprite(40, 210).makeGraphic(1086, 75, 0xFF470C60);
+    selectedOption = new FlxSprite(40, 210).makeGraphic(1086, 75, 0xFF470C60);
     insert(1, selectedOption);
 
-    for(a in 0...4){
-        noteOptions.push(new FlxText(50, 207 + (67 * a), FlxG.width, ["LEFT", 'DOWN', 'UP', "RIGHT"][a], 75));
+    for (a in 0...shitsA.length) {
+        var yPos = 207 + (67 * a);
+
+        noteOptions.push(new FlxText(50, yPos, FlxG.width, shitsB[a], 75));
         noteOptions[a].setFormat(Paths.font('vcr.ttf'), 75, 0xFFffcaec, 'left', FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
         insert(103 + a, noteOptions[a]);
 
         var controlArrayP1:Array<FlxKey> = CoolUtil.keyToString(Reflect.field(Options, 'P1_' + shitsA[a])[0]);
-        var controlArrayP2:Array<FlxKey> = CoolUtil.keyToString(Reflect.field(Options, 'P2_' + shitsA[a])[0]);
-
-        p1ControlsNotes.push(new FlxText(550, 207 + (67 * a), FlxG.width, controlArrayP1, 75));
+        p1ControlsNotes.push(new FlxText(550, yPos, FlxG.width, controlArrayP1, 75));
         p1ControlsNotes[a].setFormat(Paths.font('vcr.ttf'), 75, 0xFFffcaec, 'left', FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
         insert(50 + a, p1ControlsNotes[a]);
 
-        if(p1ControlsNotes[a].text == helper[0...helper.length]){
-            helpMe = new FlxSprite(p1ControlsNotes[a].x, p1ControlsNotes[a].y + 10).loadGraphic(Paths.image('menus/onionMenu/arrows'), true, 48, 48);
-            p1ControlsNotes[a].alpha = 0;
-            //helpMe.animation.add('arrow', [])
-            insert(2000, helpMe);
-        }
-
-        p2ControlsNotes.push(new FlxText(850, 207 + (67 * a), FlxG.width, controlArrayP2, 75));
+        var controlArrayP2:Array<FlxKey> = CoolUtil.keyToString(Reflect.field(Options, 'P2_' + shitsA[a])[0]);
+        p2ControlsNotes.push(new FlxText(850, yPos, FlxG.width, controlArrayP2, 75));
         p2ControlsNotes[a].setFormat(Paths.font('vcr.ttf'), 75, 0xFFffcaec, 'left', FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
         p2ControlsNotes[a].alpha = 0.5;
         insert(50 + a, p2ControlsNotes[a]);
@@ -71,12 +75,12 @@ function create(){
     helperArrowB = new FlxSprite(settingsPage.width + 70, settingsPage.height / 3).loadGraphic(Paths.image('menus/onionMenu/arrowSmall'));
     add(helperArrowB);
 
-    changeItem(0);
-
     FlxG.camera.zoom = 1;
+
+    changeItem(0);
 }
 
-function postCreate(){
+function postCreate() {
     controls.ACCEPT = false;
 }
 
@@ -84,68 +88,82 @@ var onP2:Bool = false;
 var allowControl:Bool = true;
 var inputRequired:Bool = false;
 
-function postUpdate(){
+function postUpdate() {
     time.text = DateTools.format(Date.now(), "%r");
-    if(allowControl){
-        if(controls.BACK) close();
 
-        if(controls.UP_P || controls.DOWN_P) changeItem((controls.UP_P ? -1 : 1));
+    if (allowControl) {
+        if (controls.BACK) close();
 
-        if(controls.LEFT_P || controls.RIGHT_P){
+        if (controls.UP_P || controls.DOWN_P)
+            changeItem((controls.UP_P ? -1 : 1));
+
+        if (controls.LEFT_P || controls.RIGHT_P) {
             onP2 = controls.RIGHT_P;
-
-            if(page == 0){
-                for(a in 0...4){
-                    p1ControlsNotes[a].alpha = onP2 ? 0.5 : 1;
-                    p2ControlsNotes[a].alpha = onP2 ? 1 : 0.5;
-                }
+            for (a in 0...shitsA.length) {
+                p1ControlsNotes[a].alpha = onP2 ? 0.5 : 1;
+                p2ControlsNotes[a].alpha = onP2 ? 1 : 0.5;
             }
         }
-        if(controls.ACCEPT){
+
+        if (controls.ACCEPT) {
             allowControl = false;
             controls.ACCEPT = false;
-            if(page == 0){
-                if(!onP2){
-                    p1ControlsNotes[optionNum].text = 'Input?';
-                    new FlxTimer().start(0.1, function(timer){
-				        inputRequired = true;
-			        });
-                } else {
-                    p2ControlsNotes[optionNum].text = 'Input?';
-                    new FlxTimer().start(0.1, function(timer){
-				        inputRequired = true;
-			        });
-                }
-            }
+
+            if (!onP2)
+                p1ControlsNotes[optionNum].text = 'Input?';
+            else
+                p2ControlsNotes[optionNum].text = 'Input?';
+
+            new FlxTimer().start(0.1, function(_) inputRequired = true);
         }
     }
 
-    if(inputRequired && FlxG.keys.justPressed.ANY){
-        if(!onP2){
-            p1ControlsNotes[optionNum].text = CoolUtil.keyToString(FlxG.keys.firstJustPressed());  
-            inputRequired = false;
-            allowControl = true;
-            Reflect.setField(Options, 'P1_' + shitsA[optionNum], [FlxG.keys.firstJustPressed()]);
-            Options.applyKeybinds();
-            Options.save();
+    if (inputRequired && FlxG.keys.justPressed.ANY) {
+        var newKey = FlxG.keys.firstJustPressed();
+        var strKey = CoolUtil.keyToString(newKey);
+
+        if (!onP2) {
+            p1ControlsNotes[optionNum].text = strKey;
+            Reflect.setField(Options, 'P1_' + shitsA[optionNum], [newKey]);
         } else {
-            p2ControlsNotes[optionNum].text = CoolUtil.keyToString(FlxG.keys.firstJustPressed());  
-            inputRequired = false;
-            allowControl = true; 
-            Reflect.setField(Options, 'P2_' + shitsA[optionNum], [FlxG.keys.firstJustPressed()]);
-            Options.applyKeybinds();
-            Options.save();
+            p2ControlsNotes[optionNum].text = strKey;
+            Reflect.setField(Options, 'P2_' + shitsA[optionNum], [newKey]);
         }
+
+        Options.applyKeybinds();
+        Options.save();
+        inputRequired = false;
+        allowControl = true;
     }
-
-
 }
 
-function changeItem(bleh){
-    if(optionNum > 2 && controls.DOWN_P) optionNum = 0;
-    else if(optionNum < 1 && controls.UP_P) optionNum = 3; 
-    else optionNum += bleh;
-    selectedOption.y = 210 + (optionNum * 66);
+function changeItem(bleh:Int) {
+    optionNum += bleh;
+    if (optionNum < 0) optionNum = shitsA.length - 1;
+    if (optionNum >= shitsA.length) optionNum = 0;
 
-    trace(onP2);
+    // scrollOffset is the topmost visible item index
+    if (optionNum < scrollOffset) scrollOffset = optionNum;
+    if (optionNum >= scrollOffset + visibleRows) scrollOffset = optionNum - visibleRows + 1;
+
+    // update visible positions
+    for (i in 0...shitsA.length) {
+        var visibleIndex = i - scrollOffset;
+        var baseY = 210 + (visibleIndex * 66);
+
+        var onScreen = visibleIndex >= 0 && visibleIndex < visibleRows;
+
+        noteOptions[i].visible = onScreen;
+        p1ControlsNotes[i].visible = onScreen;
+        p2ControlsNotes[i].visible = onScreen;
+
+        if (onScreen) {
+            noteOptions[i].y = baseY - 3;
+            p1ControlsNotes[i].y = baseY;
+            p2ControlsNotes[i].y = baseY;
+        }
+    }
+
+    // selectedOption position remains within 5 slots
+    selectedOption.y = 210 + ((optionNum - scrollOffset) * 66);
 }
