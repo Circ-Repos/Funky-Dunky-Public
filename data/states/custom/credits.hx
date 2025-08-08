4c5b5022569a88bc4717386952a75c604331a7ef
DM-kun <86197998+DMMaster636@users.noreply.github.com>
2025-08-08 16:00:47 +0200

    credits stuff - shoutouts to vscode for not letting me fix merge conflicts and having to manually do some shit (idk why it didn't work)

--- data/states/custom/credits.hx
+++ data/states/custom/credits.hx
@@ -5,7 +5,12 @@ import haxe.xml.Access;
 import Xml;
 
 var grpMenuItems:FlxTypedGroup<FlxSprite>;
-var menuItems:Array<Array<String>> = [];
+// Name[0] - Role[1] - Quote[2] - URL[3] - Portrait[4]
+var menuItems:Array<Array<String>> = [ // (Portrait at the end in case we get XML working so we can remove it)
+	["Mortal", "Lead Director", "You should probably subscribe to me on Youtube.", "https://youtu.be/", "mortal"],
+	["Mortal", "Lead Director", "You should probably subscribe to me on Youtube.", "https://youtu.be/", "mortal"],
+	["Mortal", "Lead Director", "You should probably subscribe to me on Youtube.", "https://youtu.be/", "mortal"]
+];
 var grpArrows:FlxTypedGroup<FlxSprite>;
 
 var curSelected:Int = 0;
@@ -22,15 +27,19 @@ var special:FlxSprite;
 function create()
 {
 	camFollow = new FlxObject(0, 0, 1, 1);
-	//camFollow.screenCenter();
+	camFollow.screenCenter();
 	add(camFollow);
 
 	bg = new FlxSprite().loadGraphic(Paths.image('menus/creds/bg'));
 	bg.antialiasing = Options.antialiasing;
 	bg.scrollFactor.set(0, 0);
+	bg.scale.set(1.15, 1.15);
+	bg.updateHitbox();
 	bg.screenCenter();
 	add(bg);
-	FlxG.camera.zoom = 0.2;
+
+	FlxG.camera.zoom = 0.9;
+
 	bgOverlay = new FlxBackdrop(FlxGridOverlay.createGrid(80, 80, 160, 160, true, 0xFFBBBBBB, 0xFF000000));
 	bgOverlay.scrollFactor.set(0.5, 0.5);
 	bgOverlay.antialiasing = false;
@@ -38,38 +47,62 @@ function create()
 	bgOverlay.alpha = 0.1;
 	add(bgOverlay);
 
-	boxDesc = new FlxSprite(0, 400).loadGraphic(Paths.image('menus/creds/box-desc'));
+	grpMenuItems = new FlxTypedGroup();
+	add(grpMenuItems);
+
+	boxDesc = new FlxSprite(0, 485).loadGraphic(Paths.image('menus/creds/box-desc'));
 	boxDesc.antialiasing = Options.antialiasing;
 	boxDesc.screenCenter(FlxAxes.X);
 	add(boxDesc);
 
-	grpMenuItems = new FlxTypedGroup();
-	add(grpMenuItems);
+	special = new FlxSprite().loadGraphic(Paths.image('menus/creds/special-thanks'));
+	special.antialiasing = Options.antialiasing;
+	special.scale.set(0.5, 0.5);
+	special.updateHitbox();
+	special.screenCenter();
+	add(special);
+	special.y = special.y + FlxG.height;
 
 	var num:Int = 0;
-	var xml:Access = new Access(Xml.parse(Paths.xml('config/credits')));
-	var xmlString = Paths.xml('config/credits');
-	if (xmlString == null) {
-		trace("ERROR: Could not load config/credits.xml!");
-	} else {
-		var rawXml = Xml.parse(xmlString);
-		var root = rawXml.firstElement(); // This is <menu>
-		if (root == null) {
-			trace("ERROR: Root node is null!");
-		} else {
-			for (credit in root.elementsNamed("credit")) {
-        		var node = new Access(credit);
-				var portrait:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menus/creds/' + node.att.icon));
-				portrait.antialiasing = Options.antialiasing;
-				portrait.screenCenter();
-				portrait.ID = num;
-				grpMenuItems.add(portrait);
-				menuItems.push([node.att.name, node.att.desc, node.att.quote, node.att.url]);
-				num = num + 1;
-			}
-		}
+	for (credit in menuItems) {
+    	var node = new Access(credit);
+		var portrait:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menus/creds/' + credit[4]));
+		portrait.antialiasing = Options.antialiasing;
+		portrait.scale.set(0.7, 0.7);
+		portrait.updateHitbox();
+		portrait.screenCenter();
+		portrait.x = portrait.x + FlxG.width * num;
+		portrait.y = portrait.y - 85;
+		portrait.ID = num;
+		grpMenuItems.add(portrait);
+		num = num + 1;
 	}
-	trace("Loaded XML: " + xmlString);
+
+//	var num:Int = 0;
+//	var xml:Access = new Access(Xml.parse(Paths.xml('config/credits')));
+//	var xmlString = Paths.xml('config/credits');
+//	if (xmlString == null) {
+//		trace("ERROR: Could not load config/credits.xml!");
+//	} else {
+//		var rawXml = Xml.parse(xmlString);
+//		var root = rawXml.firstElement(); // This is <menu>
+//		if (root == null) {
+//			trace("ERROR: Root node is null!");
+//		} else {
+//			for (credit in root.elementsNamed("credit")) {
+//        		var node = new Access(credit);
+//				var portrait:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menus/creds/' + node.att.icon));
+//				portrait.antialiasing = Options.antialiasing;
+//				portrait.screenCenter();
+//				portrait.ID = num;
+//				grpMenuItems.add(portrait);
+//				menuItems.push([node.att.name, node.att.desc, node.att.quote, node.att.url]);
+//				num = num + 1;
+//			}
+//		}
+//	}
+//	trace("Loaded XML: " + xmlString);
+
 	grpArrows = new FlxTypedGroup();
 	add(grpArrows);
 
@@ -81,21 +114,32 @@ function create()
 
 		var arrow:FlxSprite = new FlxSprite().loadGraphic(graphic);
 		arrow.antialiasing = Options.antialiasing;
-		if(i == 0 || i == 1) arrow.screenCenter(FlxAxes.X);
-		else arrow.screenCenter(FlxAxes.Y);
-		if(i == 0) arrow.flipX = true;
-		if(i == 3) arrow.flipY = true;
+		arrow.scrollFactor.set(0, 0);
+		if(i == 2 || i == 3)
+		{
+			arrow.y = arrow.y + 10;
+			arrow.screenCenter(FlxAxes.X);
+			if(i == 3)
+			{
+				arrow.flipY = true;
+				arrow.y = FlxG.height - arrow.height - 10;
+			}
+		}
+		else
+		{
+			arrow.x = arrow.x + 10;
+			arrow.screenCenter(FlxAxes.Y);
+			if(i == 1)
+			{
+				arrow.flipX = true;
+				arrow.x = FlxG.width - arrow.width - 10;
+			}
+		}
 		arrow.ID = i;
 		grpArrows.add(arrow);
 	}
 
-	special = new FlxSprite().loadGraphic(Paths.image('menus/creds/special-thanks'));
-	special.antialiasing = Options.antialiasing;
-	special.screenCenter();
-	add(special);
-	special.y = special.y + FlxG.height;
-
-	FlxG.camera.follow(camFollow, null, 0.06);
+	FlxG.camera.follow(camFollow, null, 0.1);
 
 	changeRow(0, false);
 	changeSelection(0, false);
@@ -105,21 +149,21 @@ function update(elapsed)
 {
 	if(!allowInputs) return;
 
-	// grpArrows.forEach(function(spr:FlxSprite)
-	// {
-	// 	if(FlxG.mouse.overlaps(spr))
-	// 	{
-	// 		var change:Int = 0;
-	// 		if(spr.ID == 1 || spr.ID == 3) change = 1;
-	// 		else change = -1;
-
-	// 		if(FlxG.mouse.justPressed)
-	// 		{
-	// 			if(spr.ID == 2 || spr.ID == 3) changeRow(change, true);
-	// 			else changeSelection(change, true);
-	// 		}
-	// 	}
-	// });
+//	grpArrows.forEach(function(spr:FlxSprite)
+//	{
+//		if(FlxG.mouse.overlaps(spr))
+//		{
+//			var change:Int = 0;
+//			if(spr.ID == 1 || spr.ID == 3) change = 1;
+//			else change = -1;
+//
+//			if(FlxG.mouse.justPressed)
+//			{
+//				if(spr.ID == 2 || spr.ID == 3) changeRow(change, true);
+//				else changeSelection(change, true);
+//			}
+//		}
+//	});
 
 	if(curRow != 1)
 	{
@@ -143,16 +187,21 @@ function changeSelection(change:Int, playSound:Bool)
 {
 	var lastSelected:Int = curSelected;
 	curSelected = FlxMath.bound(curSelected + change, 0, menuItems.length - 1);
+	grpArrows.members[0].alpha = curSelected == 0 ? 0.5 : 1;
+	grpArrows.members[1].alpha = curSelected == menuItems.length - 1 ? 0.5 : 1;
 	if(lastSelected == curSelected) return;
 
+	allowInputs = false;
+
 	if(playSound) CoolUtil.playMenuSFX(0, 0.7);
-	if(curSelected == 0) grpArrows.members[0].alpha = 0.5;
-	if(curSelected == menuItems.length - 1) grpArrows.members[1].alpha = 0.5;
 
 	grpMenuItems.forEach(function(spr:FlxSprite)
 	{
 		FlxTween.cancelTweensOf(spr);
-		FlxTween.tween(spr, {x: spr.x - (FlxG.width * change)}, 0.2, {ease: FlxEase.expoOut});
+		FlxTween.tween(spr, {x: spr.x - (FlxG.width * change)}, 0.2, {
+			ease: FlxEase.expoOut,
+			onComplete: (_) -> allowInputs = true
+		});
 	});
 }
 
@@ -160,11 +209,15 @@ function changeRow(change:Int, playSound:Bool)
 {
 	var lastRow:Int = curRow;
 	curRow = FlxMath.bound(curRow + change, 0, 1);
+	grpArrows.members[2].alpha = curRow == 0 ? 0.5 : 1;
+	grpArrows.members[3].alpha = curRow == 0 ? 1 : 0.5;
 	if(lastRow == curRow) return;
 
+	allowInputs = false;
+
 	if(playSound) CoolUtil.playMenuSFX(0, 0.7);
-	grpArrows.members[curRow + 2].alpha = 0.5;
 	camFollow.y = camFollow.y + (FlxG.height * change);
+	new FlxTimer().start(0.4, (_) -> allowInputs = true);
 }
 
 function confirmSelection()
